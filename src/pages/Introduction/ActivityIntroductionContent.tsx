0a9eca3dc9bf9e4851858fb70e76a2375786c6e1
GoldenSnich <gudtkd2005@gmail.com>
2022-08-09 21:04:59 +0900

    animation2

--- src/pages/Introduction/ActivityIntroductionContent.tsx
+++ src/pages/Introduction/ActivityIntroductionContent.tsx
@@ -3,9 +3,11 @@ import styled from 'styled-components';
 import { Col, Row } from 'antd';
 import ContentWrapper from '../../components/common/ContentWrapper';
 // import styled from 'styled-components';
-
 const Fade = require('react-reveal/Fade');
 
+const WholeDiv = styled.div`
+  width: 100vw;
+`;
 const BarRow = styled(Row)`
   margin-bottom: 100px;
   width: 100%;
@@ -13,7 +15,7 @@ const BarRow = styled(Row)`
 const BarLeft = styled.div`
   background-color: white;
   display: flex;
-  padding: 0 15px 0 15px;
+  padding: 0 15px 0 80px;
   flex-direction: row;
   width: 100%;
   height: 118px;
@@ -26,7 +28,7 @@ const BarRight = styled.div`
   background-color: white;
   display: flex;
   flex-direction: row;
-  padding: 0 15px 0 15px;
+  padding: 0 80px 0 15px;
   width: 100%;
   height: 118px;
   border-top-left-radius: 59px;
@@ -34,40 +36,65 @@ const BarRight = styled.div`
   align-items: center;
   justify-content: space-between;
 `;
+const CenterH1 = styled.h1`
+  margin: 0 0 0 0;
+  font-size: 50px;
+`;
 const GreenBall = styled.div`
   background-color: #00ff80;
   width: 80px;
   height: 80px;
   border-radius: 40px;
 `;
+const ActivityTextArr = [
+  '프로그래밍 스터디 & 프로젝트',
+  '동아리 자체 공모전 개최',
+  '각종 동아리 친목 활동 행사',
+  '프로그래밍 스터디 & 프로젝트',
+  '동아리 자체 공모전 개최',
+  '각종 동아리 친목 활동 행사',
+  '프로그래밍 스터디 & 프로젝트',
+  '동아리 자체 공모전 개최',
+  '각종 동아리 친목 활동 행사',
+];
 
 function ActivityIntroduction(): React.ReactElement {
   // 여기서 페이지 변경하는 로직 만들면 되지 않을까
-  return (
-    <ContentWrapper title="활동 소개" subTitle="비빔밥의 주요활동을 소개합니다">
-      <>
+  const ActivityDivArr: any[] = [];
+
+  for (let i = 0; i < ActivityTextArr.length; i += 1) {
+    if (i % 2 === 0) {
+      ActivityDivArr.push(
         <BarRow>
           <Col span={16}>
             <Fade left>
               <BarLeft>
-                <h1>프로그래밍 스터디 & 프로젝트</h1>
+                <CenterH1>{ActivityTextArr[i]}</CenterH1>
                 <GreenBall />
               </BarLeft>
             </Fade>
           </Col>
           <Col span={8} />
-        </BarRow>
+        </BarRow>,
+      );
+    } else {
+      ActivityDivArr.push(
         <BarRow>
           <Col span={16} offset={8}>
             <Fade right>
               <BarRight>
                 <GreenBall />
-                <h1>동아리 자체 공모전 개최</h1>
+                <CenterH1>{ActivityTextArr[i]}</CenterH1>
               </BarRight>
             </Fade>
           </Col>
-        </BarRow>
-      </>
+        </BarRow>,
+      );
+    }
+  }
+  return (
+    <ContentWrapper title="활동 소개" subTitle="비빔밥의 주요활동을 소개합니다">
+      <WholeDiv>{ActivityDivArr}</WholeDiv>
     </ContentWrapper>
   );
 }
